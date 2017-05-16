c22c685ac92f8e874d17af3096623dcc0e5b92ab
qimingxingxing <qz718@nyu.edu>
2017-05-16 11:57:17 -0400

    add decorator

--- Function/pythonDecorator.py
+++ Function/pythonDecorator.py
@@ -32,11 +32,36 @@ class Student(object):
 
 def outerFunction(func):
     def innerFunction(*args, **kwargs):
-        func(*args, **kwargs)
+        for i in args:
+            print(i)
+        func()
         print("%s" % func.__name__)
     return innerFunction
 
 @outerFunction # now = outerFunction(now) = innerFunction
-def now():
+def now(*args):
     print("5/16")
 now()
+
+def deco(func):
+    def _deco(*args, **kwargs):
+        print("before %s called." % func.__name__)
+        ret = func(*args, **kwargs)
+        print("  after %s called. result: %s" % (func.__name__, ret))
+        return ret
+    return _deco
+
+@deco
+def myfunc(a, b):
+    print(" myfunc(%s,%s) called." % (a, b))
+    return a+b
+
+@deco
+def myfunc2(a, b, c):
+    print(" myfunc2(%s,%s,%s) called." % (a, b, c))
+    return a+b+c
+
+myfunc(1, 2)
+myfunc(3, 4)
+myfunc2(1, 2, 3)
+myfunc2(3, 4, 5)
