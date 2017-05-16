e6f6e8a07a44dcc7775099a09d3dc66f88839689
qimingxingxing <qz718@nyu.edu>
2017-05-16 19:13:45 -0400

    add class

--- ClassHierarchy/ClassTest.py
+++ ClassHierarchy/ClassTest.py
@@ -1,18 +1,38 @@
 class Student(object):
+    __slots__ = ("__name", "__score")
+
     def __init__(self, name, score):
         self.__name = name
         self.__score = score
 
-    def print_score(self):
-        print("%s %s" % (self.__name, self.__score))
-
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
 
-class Teacher(object):
-    pass
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def score(self):
+        return self.__score
+
+    @name.setter
+    def score(self, score):
+        self.__score = score
+
+
+class GraduateStudent(Student):
+    __slots__ = ("__age", "__name", "__score")
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+        self.__age = 1
+
 
 qiming = Student("qiming", 100)
-qiming.hello = "hello"
-qiming.print_score()
-print(qiming.get_name())
+qiming.name = "yiming"
+print(qiming.name)
+qiming = GraduateStudent("qiming", 100)
+# qiming.__age = 1
+# 单下划线可以这么做
--- ClassHierarchy/Inheritance.py
+++ ClassHierarchy/Inheritance.py
@@ -5,13 +5,24 @@ class Animal(object):
     def eat(self):
         print("Animal is eating")
 
+    def __len__(self):
+        return 100
 
 class Dog(Animal):
     def say(self):
         print("I'm a dog")
 
-dog = Dog()
-dog.say()
+cat = Animal()
+cat.say()
+print(len(cat))
 animal = Dog()
 animal.say()
 animal.eat()
+
+def fn():
+    pass
+import types
+print(type("abc") == str)
+print(type(fn)==types.FunctionType)
+print(type(abs)==types.BuiltinFunctionType)
+print(dir(""))
